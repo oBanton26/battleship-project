58c7f127d9d9f227a15c1d3a5aee15c7a1bd403e
Ulysse <ulysse.aubanton@gmail.com>
2025-08-25 18:27:09 +0200

    Create basis for html and style

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+import { Player } from './logic/playerLogic'
+
+// Game
+const playerReal = new Player(true);
+const playerComputer = new Player(false);
+
+playerReal.gameboard.addShip([[0,0],[0,1]]);
+playerReal.gameboard.addShip([[6,6],[7,6],[8,6]]);
+playerComputer.gameboard.addShip([[9,9],[8,9]]);
+playerComputer.gameboard.addShip([[5,5],[4,5],[3,5]]);
