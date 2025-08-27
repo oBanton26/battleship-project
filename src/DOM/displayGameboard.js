2a60d2d8dc204af3ee045ef80b89f65aad0efd6c
Ulysse <ulysse.aubanton@gmail.com>
2025-08-27 14:23:59 +0200

    Build displayGameboard function

---
+++ src/DOM/displayGameboard.js
@@ -0,0 +1,23 @@
+export function displayGameboard (gameboardObject, playerGameboardClass) {
+    const gameboardDOMRef = document.querySelector(`.${playerGameboardClass}`);
+    clearGameboard(gameboardDOMRef);
+    const arrayCells = Array.from(gameboardDOMRef.querySelectorAll('.cell'));
+    const allCoordsOfShips = gameboardObject.allCoords();
+    const convertedAllCoords= allCoordsOfShips.map((value)=>{return convertCoordsIntoNumber(value)});
+    for (let coordNumb of convertedAllCoords) {
+        arrayCells[coordNumb].classList.toggle('occupied');
+    };
+}
+
+function convertCoordsIntoNumber (coord) {
+    return coord[0]*10 + coord[1];
+};
+
+function clearGameboard (gameboardDOMEl) {
+    gameboardDOMEl.textContent = '';
+    for (let i=0; i<100; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        gameboardDOMEl.appendChild(cell);
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import { Player } from './logic/playerLogic'
+import { displayGameboard } from "./DOM/displayGameboard";
 
 // Game
 const playerReal = new Player(true);
@@ -9,3 +10,6 @@ playerReal.gameboard.addShip([[0,0],[0,1]]);
 playerReal.gameboard.addShip([[6,6],[7,6],[8,6]]);
 playerComputer.gameboard.addShip([[9,9],[8,9]]);
 playerComputer.gameboard.addShip([[5,5],[4,5],[3,5]]);
+
+displayGameboard(playerReal.gameboard, 'player-gameboard');
+displayGameboard(playerComputer.gameboard, 'computer-gameboard');
