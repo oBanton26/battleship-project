d45aaadfa081b924f3c69950a35a4d601a1601dc
Ulysse <ulysse.aubanton@gmail.com>
2025-08-27 16:33:21 +0200

    Add players names

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const playerComputer = new Player(false);
 
 playerReal.gameboard.addShip([[0,0],[0,1]]);
 playerReal.gameboard.addShip([[6,6],[7,6],[8,6]]);
-playerComputer.gameboard.addShip([[9,9],[8,9]]);
+playerComputer.gameboard.addShip([[3,1],[3,2]]);
 playerComputer.gameboard.addShip([[5,5],[4,5],[3,5]]);
 
 displayGameboard(playerReal.gameboard, 'player-gameboard');
