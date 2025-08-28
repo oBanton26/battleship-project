7b3ee0ada16fde929ca68529c9e743d19710f67c
Ulysse <ulysse.aubanton@gmail.com>
2025-08-28 14:14:28 +0200

    Modify eventL to prevent from playing played spot
    I built a isValidShot that checks if the coords to attack were already
    attacked, and added that condition function to the attacking part in the
    eventListener.

--- src/DOM/eventListeners.js
+++ src/DOM/eventListeners.js
@@ -2,11 +2,26 @@ import { displayGameboard } from "./displayGameboard";
 
 export function addActionOnCells (cellEl, playerObject, pos) {
     cellEl.addEventListener('click', ()=>{
-        playerObject.gameboard.receiveAttack(convertNumberIntoCoords(pos));
+        const coords = convertNumberIntoCoords(pos);
+        if (isValidShot(coords, playerObject)) {
+            playerObject.gameboard.receiveAttack(coords);
+        } else {
+            console.log('Hey you cannot do that')
+        }
         displayGameboard(playerObject);
     })
 };
 
 export function convertNumberIntoCoords (numb) {
     return [Math.floor(numb/10), numb%10];
+};
+
+export function isValidShot (coords, playerObject) {
+    const arrayOfMissedShots = playerObject.gameboard.missedShots;
+    const isInMissedShots = arrayOfMissedShots.some((e)=>{return e[0]===coords[0] && e[1]===coords[1]});
+
+    const arrayOfGoodShots = playerObject.gameboard.goodShots;
+    const isInGoodShots = arrayOfGoodShots.some((e)=>{return e[0]===coords[0] && e[1]===coords[1]});
+    
+    return !isInMissedShots && !isInGoodShots;
 }
--- src/DOM/eventListeners.test.js
+++ src/DOM/eventListeners.test.js
@@ -1,6 +1,15 @@
-import { convertNumberIntoCoords } from "./eventListeners";
+import { Player } from "../logic/playerLogic";
+import { convertNumberIntoCoords , isValidShot } from "./eventListeners";
 
 test ('converts number into coords', ()=>{
     expect(convertNumberIntoCoords(3)).toEqual([0,3]);
     expect(convertNumberIntoCoords(20)).toEqual([2,0]);
+});
+
+test ('check if it is a valid shot for the player to play', ()=>{
+    const playerReal = new Player(true);
+    playerReal.gameboard.receiveAttack([1,6]);
+    playerReal.gameboard.receiveAttack([6,6]);
+    expect(isValidShot([1,6], playerReal)).toBe(false);
+    expect(isValidShot([0,0], playerReal)).toBe(true)
 })
